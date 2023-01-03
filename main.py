03fedca39dff5eb42170fedfbd16e212561fc650
Markian Frykman <51730258+PlanMan1717@users.noreply.github.com>
2023-01-03 14:58:01 +0000

    Cleaned up main.py and standardized indentation there. 4 Spaces is the new norm for indentation everywhere.

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from flask import Flask, request, render_template
-from flask_json import FlaskJSON, json_response
-import hashlib
+from flask import Flask, render_template
 import time
 import json
 import requests
 
+def makeRequestURL(code: str) -> str:
+    return f'https://www.wmata.com/components/stations.cfc?method=getNextTrains&StationCode={code}&returnFormat=JSON&_={int(time.time())}'
+
 def formatUnits(mins) -> str:
-  if type(mins) == int:
-    return "MIN"
-  return ""
+    if type(mins) == int:
+        return "MIN"
+    return ""
 
 OCCU_MAP_STR = {
-  0: "Free",
-  1: "Busy",
-  2: "Full"
+    0: "Free",
+    1: "Busy",
+    2: "Full"
 }
 
 def getIdFromStationName(station_name: str) -> str:
-  with open('stations.json', 'r') as f:
-    id_file = json.load(f)['stations']
-    return id_file[station_name]
+    with open('stations.json', 'r') as f:
+        id_file = json.load(f)['stations']
+        return id_file[station_name]
 
 def getCityFromStationName(station_name: str) -> str:
-  with open('stations.json', 'r') as f:
-    id_file = json.load(f)['cities']
-    return id_file[station_name]
+    with open('stations.json', 'r') as f:
+        id_file = json.load(f)['cities']
+        return id_file[station_name]
 
 app = Flask(__name__)
 FlaskJSON(app)
@@ -34,70 +35,28 @@ app.config["JSON_ADD_STATUS"] = False
 @app.route('/', methods=['GET'])
 def index():
     return render_template("credits.html")
-  #'Flask by Markian (GitHub: <a href="https://github.com/PlanMan1717/" target="_blank">@PlanMan1717</a>). Edits from Andrew (Portfolio: <a href="https://esmel.xyz/" target="_blank">@Esm&eacute;l</a>).'
-  #'Made by Markian & Andrew Frykman in 2022'
 
 
 @app.route('/hello')
 def hello():
     return 'Hey there!'
 
-
-# In the future, this should expect parameters
-@app.route('/wmata/<string:code>', methods=['GET'])
-def nextFiveLines(code):
-    # print(request.args['station'])
-    # code = request.get_json()['station']
-    time_now = int(time.time())
-    station_json = requests.get(
-        f'https://www.wmata.com/components/stations.cfc?method=getNextTrains&StationCode={code}&returnFormat=JSON&_={time_now}'
-    ).json()["TRAINS"]
-
-    out_str = ''
-  
-    for train in station_json:
-      train_string = str(train)
-      train_string = train_string.replace("'Oc': 0", "'Oc': 'Not Crowded'")
-      train_string = train_string.replace("'Oc': 1", "'Oc': 'Somewhat Crowded'")
-      train_string = train_string.replace("'Oc': 2", "'Oc': 'Crowded'")
-      
-
-      out_str += train_string  
-  
-    return '''
-  <style>
-  p {
-    /*background-color: black;*/
-    color: white;
-    font-size: 13px;
-    font-family: monospace;
-  }
-
-  body {
-    background-color: black;
-  }
-  </style>
-  <p>''' + out_str + '</p>'
-
 @app.route('/station/<string:name>')
 def makeStationHTML(name):
-  try:
-    code = getIdFromStationName(name)
-    city = getCityFromStationName(name)
-    rq = requests.get(
-        f'https://www.wmata.com/components/stations.cfc?method=getNextTrains&StationCode={code}&returnFormat=JSON&_={int(time.time())}'
-    )
-    # station_json = rq.json()["TRAINS"]
-    raw = rq.text
+    try:
+        code = getIdFromStationName(name)
+        city = getCityFromStationName(name)
+        rq = requests.get(makeRequestURL(code))
+        # station_json = rq.json()["TRAINS"]
+        raw = rq.text
+        return render_template('station.html', station=name, city=city, station_code=code, data_raw=raw)
+    except KeyError:
+        return render_template('station.html', station="This Station Doesn't Exist", station_code=":/")
 
-    
-    return render_template('station.html', station=name, city=city, station_code=code, data_raw=raw)
-  except KeyError:
-    return render_template('station.html', station="This Station Doesn't Exist", station_code=":/")
-
+# TODO: Add error handling
 @app.route('/backend/station/<string:code>')
 def backendDataGather(code):
-  pass
+    return requests.get(makeRequestURL(code)).text
 
 
 app.run(host='0.0.0.0', port=81)
