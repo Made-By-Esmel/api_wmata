4c371322016e7fdbab985a0c8bc5a10d4e15b854
Esmél <90939783+DumbSheep123@users.noreply.github.com>
2023-02-02 14:31:43 +0000

    ...

--- static/js/search.js
+++ static/js/search.js
@@ -1,12 +1,10 @@
 // Used in search.html
+
 window.onload = function () {
 const input = document.getElementById("search-input");
-if (!input) {
-    console.error("The input element could not be found");
-    return;
-}
-var suggestionsContainer = document.querySelector(".suggestions");
-var form = document.getElementById("search-form");
+  
+var suggestionsContainer = document.querySelector(".suggestions-bar");
+// var form = document.getElementById("se");
 
 input.addEventListener("input", async (event) => {
     const value = event.target.value;
@@ -16,7 +14,7 @@ input.addEventListener("input", async (event) => {
         return;
     }
 
-    var response = await fetch("https://pub-e0e86bcdb20a447ea11d8706a676342f.r2.dev/suggestions.json");
+    var response = await fetch("https://api.dumbsheep123.repl.co/suggestions.json");
     var suggestionsData = await response.json();
 
     var suggestions = suggestionsData.filter(function (suggestion) {
@@ -31,6 +29,16 @@ input.addEventListener("input", async (event) => {
     }
 
     suggestionsContainer.style.display = "block";
+    suggestionsContainer.style.userSelect = "none";
+    suggestionsContainer.style.cursor = "pointer";
+    suggestionsContainer.style.width = '50%';
+    suggestionsContainer.style.height = '200px';
+    suggestionsContainer.style.backgroundColor = 'lightgray';
+    suggestionsContainer.style.borderRadius = '5px';
+    suggestionsContainer.style.overflowY = 'auto';
+    suggestionsContainer.style.marginTop = '10px';
+    suggestionsContainer.style.fontFamily = 'Poppins';
+    suggestionsContainer.style.borderBottom = '1px solid gray';
     for (let i = 0; i < Math.min(suggestions.length, 5); i++) {
         var suggestion = suggestions[i];
         var suggestionElement = document.createElement("div");
@@ -41,7 +49,6 @@ input.addEventListener("input", async (event) => {
         suggestionElement.addEventListener("click", () => {
             input.value = suggestion;
             suggestionsContainer.style.display = "none";
-            form.submit();
         });
     }
 });
